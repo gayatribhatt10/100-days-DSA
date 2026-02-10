ecfcfc69e9df7bf1ad77244226ef077faf9fbbc8
gayatribhatt10 <gayatribhatt218@gmail.com>
2026-02-10 23:58:44 +0530

    Add files via upload

---
+++ Q1DAY9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1001];
+    int i, len;
+
+    // Read input string
+    scanf("%s", s);
+
+    len = strlen(s);
+
+    // Print characters in reverse order
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
