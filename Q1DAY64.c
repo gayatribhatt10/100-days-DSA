7e4c6d3e485d1686d340b59fdf2289005b76eb10
gayatribhatt10 <gayatribhatt218@gmail.com>
2026-04-05 20:40:45 +0530

    Add files via upload

---
+++ Q1DAY64.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adj[MAX];
+int visited[MAX];
+
+int queue[MAX], front = -1, rear = -1;
+
+void enqueue(int v) {
+    if (rear == MAX - 1) return;
+    if (front == -1) front = 0;
+    queue[++rear] = v;
+}
+
+int dequeue() {
+    if (front == -1 || front > rear) return -1;
+    return queue[front++];
+}
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+}
+
+void bfs(int s) {
+    enqueue(s);
+    visited[s] = 1;
+
+    while (front <= rear) {
+        int curr = dequeue();
+        printf("%d ", curr);
+
+        struct Node* temp = adj[curr];
+        while (temp != NULL) {
+            int adjVertex = temp->vertex;
+            if (!visited[adjVertex]) {
+                enqueue(adjVertex);
+                visited[adjVertex] = 1;
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main() {
+    int n, edges, u, v, s;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    // Initialize
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    printf("Enter number of edges: ");
+    scanf("%d", &edges);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < edges; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);       
+    }
+
+    printf("Enter source vertex: ");
+    scanf("%d", &s);
+
+    printf("BFS Traversal: ");
+    bfs(s);
+
+    return 0;
+}
